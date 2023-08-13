bf4bac2374d0808fe50ce9cabda882da7a5f689b
Gustavo <gustavo.zucolo@hotmail.com>
2023-08-13 09:58:53 -0300

    adjusted name

--- app/src/main/java/com/example/tmdbclient/data/repository/artist/datasource/ArtistLocalDataSource.kt
+++ app/src/main/java/com/example/tmdbclient/data/repository/artist/datasource/ArtistLocalDataSource.kt
@@ -4,6 +4,6 @@ import com.example.tmdbclient.data.model.Artist
 
 interface ArtistLocalDataSource {
     suspend fun getArtistsFromDB(): List<Artist>
-    suspend fun saveArtistsToDB(movies: List<Artist>)
+    suspend fun saveArtistsToDB(artists: List<Artist>)
     suspend fun clearAll()
 }
